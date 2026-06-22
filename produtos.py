fe56cc4527ca630ef9a506519dd4de9ec7c7091f
Jonathas Marrocos <jhonmarrokin@hotmail.com>
2026-06-22 18:06:37 -0300

    commit v2

--- produtos.py
+++ produtos.py
@@ -7,7 +7,7 @@ import json
 
 # Variáveis Principais
 
-produtos = []
+produtos = {}
 arq_json = r"C:\Users\Marrocos\Desktop\Repositorio CODE\cadastro de produtos\produtos.json"
 
 # Funções
@@ -32,12 +32,12 @@ def limpar_terminal():
     else:
         os.system("clear")
 
-def Barra():
+def barra():
     for n in range(101):
         i = "■" * (n * 25 // 101)
-        print(f"r\{n}% {i}", end="", flush=True)
+        print(f"\r{n}% {i}", end="", flush=True)
         sl(0.01)
-    print()
+    print("\n")
 
 def titulo(texto):
     linha = "=" * 30
@@ -53,6 +53,7 @@ def menu():
         
         try:
             opcao = int(input("Escolha uma Opção: "))
+            limpar_terminal()
             return opcao
         
         except ValueError:
@@ -61,4 +62,73 @@ def menu():
             limpar_terminal()
             continue
 
-menu()
+def cadastrar():
+    try:
+        while True:
+            print(titulo('CADASTRO'))
+            produto = (input('Produto: ')).strip().upper()
+
+            if len(produto) == 0 or not produto.isalpha():
+                print("Informe Qual produto para Cadrastar!\n")
+                parar()
+                limpar_terminal()
+                continue
+
+            if produto in produtos:
+                print('O Produto ja esta na lista!\n')
+                
+                while True:
+                    escolha = str(input('Quer adicionar a quantidade?\n[S] Sim | [N] Nao: ')).strip().upper()
+                    
+                    if escolha not in ('N', 'S'):
+                        print('Opcao Invalida!')
+                        continue
+                    break
+                
+                if escolha != "S":
+                    continue
+                
+                limpar_terminal()
+           
+            break
+        
+        while True:
+            try:
+                quantidade = int(input('Quantidade: '))
+
+                if quantidade <= 0:
+                    print('Invalido!')
+                    continue
+                break
+            except:
+                print('\nDigite apenas numeros!\n')
+                parar()
+                continue
+
+        if produto in produtos:
+            produtos[produto] += quantidade
+
+        else:
+            produtos[produto] = quantidade
+        
+        print(f'Cadastro Realizado! {produtos}')    
+        
+        #escrever_json()
+
+    except Exception as erro:
+        print(f'Erro Inesperado!\n{erro}')
+
+# Main
+
+print('Iniciando...')
+barra()
+parar()
+limpar_terminal()
+
+while True:
+    opc = menu()
+    
+    if opc == 1:
+        cadastrar()
+        parar()
+        limpar_terminal()
